20ac2045907b7d781f3763117426049627d72168
Suryakant Panda <suryakant.panda10@gmail.com>
2023-05-27 01:22:55 +0530

    4th Commit

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.example.applicationtochecklaunching.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -13,10 +16,16 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword : EditText
     private lateinit var btnLogin : Button
     private lateinit var btnSignUp : Button
+    private lateinit var mAuth: FirebaseAuth
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -28,9 +37,32 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnLogin.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
 
-
+            login(email, password)
+        }
 
     }
 
+    private fun login(email : String , password : String){
+        // logic to sign in new user
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for logging in user
+                    val intent = Intent(this@Login , MainActivity::class.java)
+                    startActivity(intent)
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@Login , "User doesn't exist", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+    }
 }
--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.chatapplication
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import com.example.applicationtochecklaunching.R
 
 class MainActivity : AppCompatActivity() {
 
